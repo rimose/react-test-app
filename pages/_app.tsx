f8600cee0ab4d3fa1954dcc9591ab93a10e9aed2
rimose <yop-ko@nate.com>
2022-12-05 19:17:58 +0900

    commit 1205

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,12 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import '../styles/globals.css';
+import type { AppProps } from 'next/app';
 import Layout from '../components/Layout';
+import 'bootstrap/dist/css/bootstrap.css';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
-  )
+    return (
+        <Layout>
+            <Component {...pageProps} />
+        </Layout>
+    );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,21 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { useEffect, useState } from 'react'
-import styles from '../styles/Home.module.css'
+import homeStyle from '../styles/Home.module.css';
+import Login from './Login';
+import cx from 'classnames';
 
 export default function Home() {
-  const [time, setTime] = useState('');
-  useEffect(() => {
-    setTime(new Date().toISOString());
-  }, [])
-  return (
-    <div className={styles.container}>
-        <h1 className='title'>
-          {time}
-        </h1>
-    </div>
-  )
+    return (
+        <div className={cx(homeStyle.main, 'container ')}>
+            <div className={cx('text-center', 'title-box')}>
+                <h1>사물함</h1>
+                <h4>전하지 못한 진심을 상대에게 전달해보세요</h4>
+            </div>
+
+            <div className="row">
+                <div style={{ background: 'skyblue' }}>
+                    학창시절 사물함에 몰래 편지를 넣어본적 있나요? 해당 사이트는 학창시절의 사물함에 영감을 받아 유저의
+                    전화번호와 이름을 기반으로 전하지 못한 진심을 전할 수 있습니다. x
+                </div>
+            </div>
+        </div>
+    );
 }
